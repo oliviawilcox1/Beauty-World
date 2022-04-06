ee647b26ad32754ea5f477e214bc8a9e4eb19034
oliviawilcox1 <oliviawilcox07@gmail.com>
2022-04-06 11:28:45 -0400

    Edit file nsaved

--- src/components/reviews/EditReview.js
+++ src/components/reviews/EditReview.js
@@ -1,80 +1,78 @@
-import React, { useState } from 'react';
-import { Modal } from 'react-bootstrap';
-import ReviewForm from '../shared/ToyForm';
-import { updateReview } from '../../api/toys.js';
+import React, { useState } from 'react'
+import { Modal } from 'react-bootstrap'
+import ToyForm from '../shared/ToyForm'
+import {updateToy} from '../../api/toys.js'
 
-const EditReviewModal = (props) => {
-  const { user, pet, show, handleClose, msgAlert, triggerRefresh } = props;
-  const [toy, setToy] = useState(props.toy);
+const EditToyModal = (props) => {
+    const { user, pet, show, handleClose, msgAlert, triggerRefresh } = props
+    const [toy, setToy] = useState(props.toy)
 
-  const handleChange = (e) => {
-    // e === event
-    e.persist();
+    const handleChange = (e) => {
+        // e === event
+        e.persist()
 
-    setToy((prevToy) => {
-      const name = e.target.name;
-      let value = e.target.value;
-      console.log('etarget type', e.target.type);
-      console.log('this is e.target checked', e.target.checked);
-      if (name === 'isSqueaky' && e.target.checked) {
-        value = true;
-      } else if (name === 'isSqueaky' && !e.target.checked) {
-        value = false;
-      }
+        setToy(prevToy => {
+            const name = e.target.name
+            let value = e.target.value
+            console.log('etarget type', e.target.type)
+            console.log('this is e.target checked', e.target.checked)
+            if(name === "isSqueaky" && e.target.checked){
+                value = true
+            } else if (name === "isSqueaky" && !e.target.checked){
+                value = false
+            }
 
-      if (e.target.type === 'number') {
-        value = parseInt(e.target.value);
-      }
+            if (e.target.type === 'number') {
+                value = parseInt(e.target.value)
+            }
 
-      const updatedValue = { [name]: value };
+            const updatedValue = { [name]: value }
 
-      console.log('prevToy', prevToy);
-      console.log('updatedValue', updatedValue);
+            console.log('prevToy', prevToy)
+            console.log('updatedValue', updatedValue)
 
-      return { ...prevToy, ...updatedValue };
-    });
-  };
-
-  const handleSubmit = (e) => {
-    // e === event
-    e.preventDefault();
-
-    console.log('the toy to submit', toy);
-    updateToy(user, pet.id, toy._id, toy)
-      // if create is successful, we should navigate to the show page
-      .then(() => handleClose())
-      // then we send a success message
-      .then(() =>
-        msgAlert({
-          heading: 'Toy updated!',
-          message: 'great! the pet loves it!',
-          variant: 'success',
+            return {...prevToy, ...updatedValue}
         })
-      )
-      .then(() => triggerRefresh())
-      // if there is an error, we'll send an error message
-      .catch(() =>
-        msgAlert({
-          heading: 'Oh No!',
-          message: 'that aint it',
-          variant: 'danger',
-        })
-      );
-  };
+    }
 
-  return (
-    <Modal show={show} onHide={handleClose}>
-      <Modal.Header closeButton></Modal.Header>
-      <Modal.Body>
-        <ToyForm
-          toy={toy}
-          handleChange={handleChange}
-          handleSubmit={handleSubmit}
-          heading="Give pet a toy!"
-        />
-      </Modal.Body>
-    </Modal>
-  );
-};
+    const handleSubmit = (e) => {
+        // e === event
+        e.preventDefault()
 
-export default EditToyModal;
+        console.log('the toy to submit', toy)
+        updateToy(user, pet.id, toy._id, toy)
+            // if create is successful, we should navigate to the show page
+            .then(() => handleClose())
+            // then we send a success message
+            .then(() =>
+                msgAlert({
+                    heading: 'Toy updated!',
+                    message: 'great! the pet loves it!',
+                    variant: 'success',
+                }))
+            .then(() => triggerRefresh())
+            // if there is an error, we'll send an error message
+            .catch(() =>
+                msgAlert({
+                    heading: 'Oh No!',
+                    message: 'that aint it',
+                    variant: 'danger',
+                }))
+    }
+
+    return (
+        <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton></Modal.Header>
+            <Modal.Body>
+                <ToyForm
+                    toy={toy}
+                    handleChange={handleChange}
+                    handleSubmit={handleSubmit}
+                    heading="Give pet a toy!"
+                />
+            </Modal.Body>
+        </Modal>
+    )
+}
+
+export default EditToyModal
