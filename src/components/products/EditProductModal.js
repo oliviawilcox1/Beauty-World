c6cf36993c5c273bc39ce035860a7cbe1d9709b9
oliviawilcox1 <oliviawilcox07@gmail.com>
2022-07-20 19:22:41 -0400

    EditProductModal Cleaned and Updated

--- src/components/products/EditProductModal.js
+++ src/components/products/EditProductModal.js
@@ -3,52 +3,26 @@ import { Modal } from 'react-bootstrap';
 import ProductForm from '../shared/ProductForm';
 
 const EditProductModal = (props) => {
-  const { user, show, handleClose, updateProduct, msgAlert, triggerRefresh } =
-    props;
+  const { user, show, handleClose, updateProduct, msgAlert, triggerRefresh } = props;
   const [product, setProduct] = useState(props.product);
-
   const handleChange = (e) => {
-    // e === event
     e.persist();
-
     setProduct((prevProduct) => {
       const key = e.target.name;
       let value = e.target.value;
-      console.log('etarget type', e.target.type);
-      console.log('this is e.target checked', e.target.checked);
-
       if (e.target.type === 'number') {
         value = parseInt(e.target.value);
       }
-      
-
       const updatedValue = { [key]: value };
-
-      console.log('prevProduct', prevProduct);
-      console.log('updatedValue', updatedValue);
-
       return { ...prevProduct, ...updatedValue };
     });
   };
 
   const handleSubmit = (e) => {
-    // e === event
     e.preventDefault();
-
-    console.log('the product to submit', product);
     updateProduct(user, product)
-      // if create is successful, we should navigate to the show page
       .then(() => handleClose())
-      // then we send a success message
-      .then(() =>
-        msgAlert({
-          heading: 'Product Updated! Success!',
-          message: 'u did it',
-          variant: 'success',
-        })
-      )
       .then(() => triggerRefresh())
-      // if there is an error, we'll send an error message
       .catch(() =>
         msgAlert({
           heading: 'Oh No!',
@@ -56,7 +30,6 @@ const EditProductModal = (props) => {
           variant: 'danger',
         })
       );
-    console.log('this is the product', product);
   };
 
   return (
@@ -67,7 +40,7 @@ const EditProductModal = (props) => {
           product={product}
           handleChange={handleChange}
           handleSubmit={handleSubmit}
-          heading="Edit product!"
+          heading="Edit Product!"
         />
       </Modal.Body>
     </Modal>
--- src/components/products/IndexProducts.js
+++ src/components/products/IndexProducts.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react'
 import { getAllProducts } from '../../api/products'
 import { Link } from 'react-router-dom'
 
-
 const cardContainerLayout = {
     display: 'flex',
     justifyContent: 'space-between',
