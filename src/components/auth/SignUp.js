4e73a424052001e233f30d1045bdee90375537f5
oliviawilcox1 <oliviawilcox07@gmail.com>
2022-07-20 19:00:51 -0400

    SignUp Cleaned and Updated

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -21,16 +21,11 @@ const SignUp = (props) => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [passwordConfirmation, setPasswordConfirmation] = useState('')
-
     const navigate = useNavigate()
-
 	const onSignUp = (event) => {
 		event.preventDefault()
-
 		const { msgAlert, setUser } = props
-
         const credentials = {email, password, passwordConfirmation}
-
 		signUp(credentials)
 			.then(() => signIn(credentials))
 			.then((res) => setUser(res.data.user))
@@ -55,52 +50,50 @@ const SignUp = (props) => {
 	}
 
 
-    return (
-        <div className='row'>
-            <div className='col-sm-10 col-md-8 mx-auto mt-5'>
-                <h3>Sign Up</h3>
-                <Form onSubmit={onSignUp}>
-                    <Form.Group controlId='email'>
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control
-                            required
-                            type='email'
-                            name='email'
-                            value={email}
-                            placeholder='Enter email'
-                            onChange={e => setEmail(e.target.value)}
-                        />
-                    </Form.Group>
-                    <Form.Group controlId='password'>
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control
-                            required
-                            name='password'
-                            value={password}
-                            type='password'
-                            placeholder='Password'
-                            onChange={e => setPassword(e.target.value)}
-                        />
-                    </Form.Group>
-                    <Form.Group controlId='passwordConfirmation'>
-                        <Form.Label>Password Confirmation</Form.Label>
-                        <Form.Control
-                            required
-                            name='passwordConfirmation'
-                            value={passwordConfirmation}
-                            type='password'
-                            placeholder='Confirm Password'
-                            onChange={e => setPasswordConfirmation(e.target.value)}
-                        />
-                    </Form.Group>
-                    <Button variant='primary' type='submit'>
-                        Submit
-                    </Button>
-                </Form>
-            </div>
+return (
+    <div className='row'>
+        <div className='col-sm-10 col-md-8 mx-auto mt-5'>
+            <h3>Sign Up</h3>
+            <Form onSubmit={onSignUp}>
+                <Form.Group controlId='email'>
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control
+                        required
+                        type='email'
+                        name='email'
+                        value={email}
+                        placeholder='Enter email'
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                </Form.Group>
+                <Form.Group controlId='password'>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        required
+                        name='password'
+                        value={password}
+                        type='password'
+                        placeholder='Password'
+                        onChange={e => setPassword(e.target.value)}
+                    />
+                </Form.Group>
+                <Form.Group controlId='passwordConfirmation'> 
+                    <Form.Label>Password Confirmation</Form.Label>
+                    <Form.Control
+                        required
+                        name='passwordConfirmation'
+                        value={passwordConfirmation}
+                        type='password'
+                        placeholder='Confirm Password'
+                        onChange={e => setPasswordConfirmation(e.target.value)}
+                    />
+                </Form.Group>
+                <Button variant='primary' type='submit'>
+                    Submit
+                </Button>
+            </Form>
         </div>
-    )
-
-}
+    </div>
+)}
 
 export default SignUp
